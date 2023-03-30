f9c515865c4db78912d8986e0c1877fec0f8d4fb
Neil-Chen <v009008777@hotmail.com>
2023-03-30 09:35:16 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## File strucutre
+
+- `requestHandler.ts` - The main file that handles the requests from chrome extension, use `express.js` to serve the requests.
+- `main.ts` - Obsidian settings.
+
+## Usage 
+
+See parent directory.
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+import { OmnisearchHttpSettings } from "./types";
+
+export const DEFAULT_SETTINGS: OmnisearchHttpSettings = {
+    port: 27080,
+};
+
+export const HOSTNAME = "127.0.0.1";
+
+export const PLUGIN_NAME = "omnisearch-http";
